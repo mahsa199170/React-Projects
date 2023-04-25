2f480910cd567a8c4dd957890360a834d99e6b3f
mahsa199170 <mahsa.javady@gmail.com>
2023-04-24 22:56:52 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GithubUsersList from './components/GithubUsersList'
+
+const App = () => {
+  return (
+    <div>
+      <GithubUsersList />
+    </div>
+  )
+}
+
+export default App
